e6c67cc4d85ecf2101d000dc12394e0075741009
Zayle <76919230+veryhungryprogrammer@users.noreply.github.com>
2023-02-26 17:35:42 -0800

    King Gets Bigger

--- Hunger/Assets/Scripts/King.cs
+++ Hunger/Assets/Scripts/King.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class King : MonoBehaviour
 {
-    [SerializeField] private int _health = 50;
+    [SerializeField] private float _health = 50;
+    private float _maxHealth = 50;
     
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,6 +15,13 @@ public class King : MonoBehaviour
             {
                 GlobalData.TimeLeft = GlobalData.currentGameTimeReset;
                 _health -= 5;
+
+                float newScale = 0.1f;
+                this.transform.localScale += new Vector3(newScale, newScale, newScale);
+
+                float newYpos = 0.105f;
+                this.transform.position += new Vector3(0, newYpos, 0);
+
                 Destroy(collision.gameObject);
             }
             else if (GlobalData.currentPlayerFood == "lethalFood")
@@ -21,6 +29,13 @@ public class King : MonoBehaviour
                 GlobalData.currentGameTimeReset += GlobalData.lethalCurrentTimeIncrease;
                 GlobalData.TimeLeft = GlobalData.currentGameTimeReset;
                 _health -= 10;
+
+                float newScale = 0.2f;
+                this.transform.localScale += new Vector3(newScale, newScale, newScale);
+
+                float newYpos = 0.21f;
+                this.transform.position += new Vector3(0, newYpos, 0);
+
                 Destroy(collision.gameObject);
             }
 
